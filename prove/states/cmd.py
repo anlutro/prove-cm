beb5abc6d62349c8ed2541573f260f9fe1ab1ffd
Andreas Lutro <anlutro@gmail.com>
2015-12-11 23:32:46 +0100

    don't call format unless necessary

--- prove/states/cmd.py
+++ prove/states/cmd.py
@@ -3,9 +3,9 @@ from prove.states import State
 
 class Run(State):
 	def run(self, command=None, format_vars=None):
-		if format_vars is None:
-			format_vars = {}
-		result = self._run_command(command.format(**format_vars))
+		if format_vars is not None:
+			command = command.format(**format_vars)
+		result = self._run_command(command)
 		if result.was_successful:
 			return True, result.stdout
 		return False, result.stderr
