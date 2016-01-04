71443b5c0772caa5d4e551b215e384e55660011b
Andreas Lutro <anlutro@gmail.com>
2016-01-04 20:09:20 +0100

    add a cmd state test

---
+++ tests/unit/states/cmd_test.py
@@ -0,0 +1,16 @@
+from unittest import mock
+from prove.states import cmd
+
+
+def test_run():
+	cmd_result = mock.Mock()
+	cmd_result.was_successful = True
+	cmd_result.stdout = 'stdout'
+	cmd_result.stderr = 'stderr'
+	conn = mock.Mock()
+	conn.run_command = mock.Mock(return_value=cmd_result)
+
+	state_result = cmd.run(conn, {'command': 'fake_command'})
+	conn.run_command.assert_called_once_with('fake_command')
+	assert True == state_result.success
+	assert 'STDERR:\nstderr\nSTDOUT:\nstdout' == state_result.comment
