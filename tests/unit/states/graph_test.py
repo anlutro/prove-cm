69424d694b071c080e91305d50ac9ca3376a4cfc
Andreas Lutro <anlutro@gmail.com>
2017-02-08 20:12:20 +0100

    clean up tests

--- tests/unit/states/graph_test.py
+++ tests/unit/states/graph_test.py
@@ -2,39 +2,79 @@ import pytest
 from prove.states import State, StateFuncCall
 from prove.states.graph import generate_graph
 
+
+def _state(name, requires=None):
+	kwargs = {'requires': requires} if requires else {}
+	return State(name, [StateFuncCall('test.noop', kwargs)])
+
+
+def test_dependency():
+	graph = generate_graph([_state('a'), _state('b', ['a'])])
+	assert 'a -> b' == str(graph)
+
+
+def test_multiple_dependents():
+	graph = generate_graph([
+		_state('a'),
+		_state('b', ['a']),
+		_state('c', ['a']),
+	])
+	assert 'a -> (b+c)' == str(graph)
+
+
 def test_singular_requirements():
-	states = [
-		State('a', [StateFuncCall('test.noop', {})]),
-		State('b', [StateFuncCall('test.noop', {'requires': ['a']})]),
-		State('c', [StateFuncCall('test.noop', {'requires': ['a']})]),
-		State('d', [StateFuncCall('test.noop', {'requires': ['c']})]),
-	]
-	graph = generate_graph(states)
+	graph = generate_graph([
+		_state('a'),
+		_state('b', ['a']),
+		_state('c', ['a']),
+		_state('d', ['c']),
+	])
 	assert 'a -> (b + (c -> d))' == str(graph)
 
 
-@pytest.mark.skip(reason='not implemented yet')
+@pytest.mark.xfail(reason='not implemented yet')
+def test_multiple_dependencies():
+	graph = generate_graph([
+		_state('a'),
+		_state('b',),
+		_state('c', ['a', 'b']),
+	])
+	assert '(a+b) -> c' == str(graph)
+
+
+@pytest.mark.xfail(reason='not implemented yet')
 def test_mutliple_requirements():
-	states = [
-		State('a', [StateFuncCall('test.noop', {})]),
-		State('b', [StateFuncCall('test.noop', {})]),
-		State('c', [StateFuncCall('test.noop', {'requires': ['a', 'b']})]),
-		State('d', [StateFuncCall('test.noop', {'requires': ['c']})]),
-		State('e', [StateFuncCall('test.noop', {'requires': ['c']})]),
-		State('f', [StateFuncCall('test.noop', {'requires': ['d', 'e']})]),
-	]
-	graph = generate_graph(states)
+	graph = generate_graph([
+		_state('a'),
+		_state('b'),
+		_state('c', ['a', 'b']),
+		_state('d', ['c']),
+		_state('e', ['c']),
+		_state('f', ['d', 'e']),
+	])
 	assert '(a+b) -> c -> (d+e) -> f' == str(graph)
 
 
+@pytest.mark.xfail(reason='not implemented yet')
+def test_shared_branches():
+	graph = generate_graph([
+		_state('a'),
+		_state('b', ['a']),
+		_state('c', ['a']),
+		_state('d', ['a']),
+		_state('e', ['b', 'c']),
+		_state('f', ['c', 'd']),
+	])
+	assert 'a -> (b+c+d) -> (e+f)' == str(graph)
+
+
 def test_multiple_parallel():
-	states = [
-		State('a', [StateFuncCall('test.noop', {})]),
-		State('b', [StateFuncCall('test.noop', {})]),
-		State('c', [StateFuncCall('test.noop', {'requires': ['a']})]),
-		State('d', [StateFuncCall('test.noop', {'requires': ['b']})]),
-		State('e', [StateFuncCall('test.noop', {'requires': ['c']})]),
-		State('f', [StateFuncCall('test.noop', {'requires': ['d']})]),
-	]
-	graph = generate_graph(states)
+	graph = generate_graph([
+		_state('a'),
+		_state('b'),
+		_state('c', ['a']),
+		_state('d', ['b']),
+		_state('e', ['c']),
+		_state('f', ['d']),
+	])
 	assert 'a -> c -> e\nb -> d -> f' == str(graph)
