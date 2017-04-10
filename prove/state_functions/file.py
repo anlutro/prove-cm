59567c3aa1eb712ee8056e4d9b9cb6476cd70d89
Andreas Lutro <anlutro@gmail.com>
2017-04-10 20:22:56 +0200

    fix issues with file state

--- prove/state_functions/file.py
+++ prove/state_functions/file.py
@@ -8,7 +8,7 @@ class FileState:
 		self.session = session
 
 	def upload_tmpfile(self, source):
-		result = self.session.run_command('mktemp --suffix=.prove')
+		result = self.session.run_command('mktemp --suffix=.prove', skip_sudo=True)
 		tmp_path = result.stdout.strip()
 		self.session.upload_file(source, tmp_path)
 		return tmp_path
@@ -19,11 +19,8 @@ class FileState:
 
 	def get_diff(self, path1, path2):
 		def get_file_lines(path):
-			if self.session.run_command('test -f {}'.format(path)).was_successful:
-				result = self.session.run_command('cat {}'.format(path))
-				return result.stdout.splitlines()
-			else:
-				return []
+			result = self.session.run_command('cat {}'.format(path))
+			return result.stdout.splitlines()
 
 		diff = difflib.unified_diff(get_file_lines(path1), get_file_lines(path2))
 		if diff:
@@ -32,27 +29,51 @@ class FileState:
 		return diff
 
 	def move_tmpfile(self, tmpfile, path):
-		return self.session.run_command('mv {} {}'.format(tmpfile, path))
+		return self.session.run_command('mv %s %s' % (tmpfile, path))
+
+	def _stat(self, path, fmt):
+		result = self.session.run_command("stat -c '%s' %s" % (fmt, path))
+		return result.stdout.strip()
 
 	def ensure_user(self, path, user):
-		raise NotImplementedError()
+		current_user = self._stat(path, '%U')
+		if current_user != user:
+			self.session.run_command('chown %s %s' % (user, path))
+		return current_user, user
 
 	def ensure_group(self, path, group):
-		raise NotImplementedError()
+		current_group = self._stat(path, '%G')
+		if current_group != group:
+			self.session.run_command('chgrp %s %s' % (group, path))
+		return current_group, group
 
 	def ensure_mode(self, path, mode):
-		raise NotImplementedError()
+		current_mode = self._stat(path, '%a')
+		if current_mode != mode:
+			self.session.run_command('chmod %s %s' % (mode, path))
+		return current_mode, mode
 
 
 class FileManagedState(FileState):
-	def run(self, path, source=None, user=None, group=None, mode=None):
+	def run(self, path, source=None, content=None, user=None, group=None, mode=None):
 		result = StateResult()
 		result.changes = []
 
+		file_exists = self.session.run_command('test -f %s' % path).exit_code == 0
+
 		if source is not None:
 			tmpfile = self.upload_tmpfile(source)
-			result.changes.extend(self.get_diff(path, tmpfile))
-			mv_result = self.move_tmpfile(tmpfile, path)
+			should_move_file = False
+			if file_exists:
+				diff = self.get_diff(path, tmpfile)
+				if diff:
+					result.changes.extend(diff)
+					should_move_file = True
+			else:
+				should_move_file = True
+				result.changes.append('file created: %s' % path)
+			if should_move_file:
+				mv_result = self.move_tmpfile(tmpfile, path)
 
 		if user is not None:
 			old_user, new_user = self.ensure_user(path, user)
@@ -67,7 +88,7 @@ class FileManagedState(FileState):
 		if mode is not None:
 			old_mode, new_mode = self.ensure_mode(path, mode)
 			if old_mode != new_mode:
-				result.changes.append('Mode changed from {} to {}'.format(old_mode, new_mode))
+				result.changes.append('mode changed from {} to {}'.format(old_mode, new_mode))
 
 		result.success = True
 		return result
