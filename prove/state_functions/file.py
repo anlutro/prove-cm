71596a83c7137546d80c972006bc3debe64badad
Andreas Lutro <anlutro@gmail.com>
2017-04-11 14:25:05 +0200

    add more file states

--- prove/state_functions/file.py
+++ prove/state_functions/file.py
@@ -1,12 +1,10 @@
 import difflib
 
+from prove.state_functions import AbstractState
 from prove.states import StateResult
 
 
-class FileState:
-	def __init__(self, session):
-		self.session = session
-
+class FileState(AbstractState):
 	def write_tmpfile(self, source=None, content=None):
 		result = self.session.run_command('mktemp --suffix=.prove', skip_sudo=True)
 		tmp_path = result.stdout.strip()
@@ -56,7 +54,7 @@ class FileState:
 		return current_mode, mode
 
 
-class FileManagedState(FileState):
+class ManagedState(FileState):
 	def run(self, path, source=None, content=None, user=None, group=None, mode=None):
 		result = StateResult()
 		result.changes = []
@@ -76,7 +74,7 @@ class FileManagedState(FileState):
 					should_move_file = True
 			else:
 				should_move_file = True
-				result.changes.append('file created: %s' % path)
+				result.changes.append('created file %r' % path)
 			if should_move_file:
 				self.move_tmpfile(tmpfile, path)
 
@@ -96,10 +94,62 @@ class FileManagedState(FileState):
 				result.changes.append('mode changed from {} to {}'.format(old_mode, new_mode))
 
 		if not result.changes:
-			result.comment = 'no changes to file %s' % path
+			result.comment = 'no changes to file %r' % path
 		result.success = True
 		return result
 
 
+class DirectoryState(FileState):
+	def run(self, path, user=None, group=None, mode=None):
+		result = StateResult()
+		result.changes = []
+
+		dir_exists = self.session.run_command('test -d "%s"' % path).was_successful
+		if not dir_exists:
+			self.session.run_command('mkdir -p "%s"' % path)
+			result.changes.append('created directory %r' % path)
+
+		if user is not None:
+			old_user, new_user = self.ensure_user(path, user)
+			if old_user != new_user:
+				result.changes.append('user changed from {} to {}'.format(old_user, new_user))
+
+		if group is not None:
+			old_group, new_group = self.ensure_group(path, group)
+			if old_group != new_group:
+				result.changes.append('group changed from {} to {}'.format(old_group, new_group))
+
+		if mode is not None:
+			old_mode, new_mode = self.ensure_mode(path, mode)
+			if old_mode != new_mode:
+				result.changes.append('mode changed from {} to {}'.format(old_mode, new_mode))
+
+		if not result.changes:
+			result.comment = 'no changes to directory %r' % path
+		result.success = True
+		return result
+
+
+class AbsentState(FileState):
+	def run(self, path):
+		result = StateResult(success=True)
+		exists = self.session.run_command('test -e "%s"' % path).was_successful
+		if not exists:
+			result.comment = 'file already absent: %r'
+			return result
+
+		rm = self.session.run_command('rm -rfv "%s"' % path)
+		result.merge_with_cmd_result(rm)
+		return result
+
+
 def managed(session, args):
-	return FileManagedState(session).run(**args)
+	return ManagedState(session).run(**args)
+
+
+def directory(session, args):
+	return DirectoryState(session).run(**args)
+
+
+def absent(session, args):
+	return AbsentState(session).run(**args)
