45bb7075dc5765b7db07ea1c1139088cae71882e
Andreas Lutro <anlutro@gmail.com>
2017-04-15 00:24:05 +0200

    use __call__ to reduce duplication

--- prove/output/composite.py
+++ prove/output/composite.py
@@ -6,29 +6,5 @@ class CompositeOutput:
 		for outputter in self.outputters:
 			getattr(outputter, func)(*args, **kwargs)
 
-	def connect_start(self, *args, **kwargs):
-		self._forward('connect_start', args, kwargs)
-
-	def connect_success(self, *args, **kwargs):
-		self._forward('connect_success', args, kwargs)
-
-	def connect_failure(self, *args, **kwargs):
-		self._forward('connect_failure', args, kwargs)
-
-	def disconnected(self, *args, **kwargs):
-		self._forward('disconnected', args, kwargs)
-
-	def cmd_result(self, *args, **kwargs):
-		self._forward('cmd_result', args, kwargs)
-
-	def state_fncall_start(self, *args, **kwargs):
-		self._forward('state_fncall_start', args, kwargs)
-
-	def state_fncall_finish(self, *args, **kwargs):
-		self._forward('state_fncall_finish', args, kwargs)
-
-	def state_summary(self, *args, **kwargs):
-		self._forward('state_summary', args, kwargs)
-
-	def comment(self, *args, **kwargs):
-		self._forward('comment', args, kwargs)
+	def __call__(self, func, *args, *kwargs):
+		self._forward(func, args, kwargs)
--- prove/output/remote.py
+++ prove/output/remote.py
@@ -12,29 +12,5 @@ class RemoteOutput:
 			'kwargs': prove.remote.serialize(kwargs),
 		})
 
-	def connect_start(self, *args, **kwargs):
-		self._send('connect_start', args, kwargs)
-
-	def connect_success(self, *args, **kwargs):
-		self._send('connect_success', args, kwargs)
-
-	def connect_failure(self, *args, **kwargs):
-		self._send('connect_failure', args, kwargs)
-
-	def disconnected(self, *args, **kwargs):
-		self._send('disconnected', args, kwargs)
-
-	def cmd_result(self, *args, **kwargs):
-		self._send('cmd_result', args, kwargs)
-
-	def state_fncall_start(self, *args, **kwargs):
-		self._send('state_fncall_start', args, kwargs)
-
-	def state_fncall_finish(self, *args, **kwargs):
-		self._send('state_fncall_finish', args, kwargs)
-
-	def state_summary(self, *args, **kwargs):
-		self._send('state_summary', args, kwargs)
-
-	def comment(self, *args, **kwargs):
-		self._send('comment', args, kwargs)
+	def __call__(self, func, *args, *kwargs):
+		self._send(func, args, kwargs)
